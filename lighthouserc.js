fd5c1847c551caec2f82cebddca891e8690f62a4
tomas f <46322567+TomFern@users.noreply.github.com>
2022-10-26 11:39:48 -0300

    update

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,12 +6,12 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', {minScore: 1}],
-        'categories:accessibility': ['error', {minScore: 1}]
+        'categories:performance': ['error', {minScore: 0.9}],
+        'categories:accessibility': ['error', {minScore: 0.9}],
+        'categories:seo': ['error', {minScore: 0.9}]
         }
     },
     upload: {
-      // target: 'temporary-public-storage',
       target: 'filesystem',
       outputDir: 'reports'
     },
