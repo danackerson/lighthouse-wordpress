91292be020c8fe093c0242e560ce3bfc50cbdc87
tomas f <46322567+TomFern@users.noreply.github.com>
2022-10-28 09:24:56 -0300

    update assertions

--- audit.test.js
+++ audit.test.js
@@ -1,28 +1,24 @@
 const fs = require("fs");
-// const { describe, it } = require("node:test");
-//const { default: test } = require("node:test");
-
 const reportFile = "reports/manifest.json";
-
 const report = JSON.parse(fs.readFileSync(reportFile));
 
 report.forEach(r => {
     let summary = r.summary;
     describe('Performance', () => {
-        it('should be at least 40%', () => {
-            expect(summary.performance).toBeGreaterThanOrEqual(0.4);
+        it('should be at least 90%', () => {
+            expect(summary.performance).toBeGreaterThanOrEqual(0.9);
         });
     });
     
     describe('Accessibility', () => {
-        it('should be at least 40%', () => {
-            expect(summary.accessibility).toBeGreaterThanOrEqual(0.4);
+        it('should be at least 90%', () => {
+            expect(summary.accessibility).toBeGreaterThanOrEqual(0.9);
         });
     });
     
     describe('SEO', () => {
-        it('should be at least 40%', () => {
-            expect(summary.accessibility).toBeGreaterThanOrEqual(0.4);
+        it('should be at least 90%', () => {
+            expect(summary.accessibility).toBeGreaterThanOrEqual(0.9);
         });
     });
     
